480fc9c6161d46669419a107954619f03c966d8b
Shehryar <shehryar2212@yahoo.com>
2016-03-06 21:51:12 -0500

    views, turned class based, url need to be class based

--- posts/views.py
+++ posts/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render, redirect
+from django.views.generic import View
+
 from posts.models import Post
 from .forms import PostForm,UserForm,UserProfileForm
 from django.shortcuts import get_object_or_404
 
 #user registration
 class register(View):
-	template = 'register.html'
-def get(self,request):
-	register = False
-	if request.user.is_authenticated():
-		return render(request,"index.html", {})
+	template = "register.html"
+	def get(self,request):
+		register = False
+		if request.user.is_authenticated():
+			return render(request,"index.html", {})
 
-	userform = UserForm()
-	profileform = UserProfileForm()
+		userform = UserForm()
+		profileform = UserProfileForm()
 
-	context={
-	'userform':userform,
-	'profileform':profileform
-	}
+		context={
+		'userform':userform,
+		'profileform':profileform
+		}
 
-	return render(request,self.template, context)
+		return render(request,self.template, context)
 
-	if request.method =="POST":
+	def post(self,request):
 
 		userform = UserForm(data=request.POST)
 		profileform = UserProfileForm(data=request.POST)
@@ -40,35 +42,39 @@ def get(self,request):
 			'profileform':profileform
 			}
 
-			return render(request,'register.html',context)
+			return render(request,self.template,context)
 
-def login(request):
-	if request.method =="GET":
-		return render(request,'login.html',{})
-	if request.method =="POST":
-		username = request.POST['username']
-		password = request.POST['password']
+# class login(self,request):
+
+# 	if request.method =="GET":
+# 		return render(request,'login.html',{})
+# 	if request.method =="POST":
+# 		username = request.POST['username']
+# 		password = request.POST['password']
 
 
 
-# Create your views here.
-def index(request):
-	#rose for commenting 
-	blog_list = Post.objects.all().order_by('-updated_at')
+class index(View):
+	def get(self,request):
+		template = "index.html"
+		#rose for commenting 
+		blog_list = Post.objects.all().order_by('-updated_at')
 
-	context = {
-		"posts" : blog_list
-	}
-	return render(request, 'index.html', context)
+		context = {
+			"posts" : blog_list
+		}
+		return render(request, 'index.html', context)
 
-#rose for commenting
-def create(request):
-	if request.method == "GET":
+class create(View):
+	template = "create.html"
+
+	def get(self,request):
 		context = {
 		"form":PostForm()
 		}
-		return render(request, 'create.html', context)
-	elif request.method =="POST":
+		return render(request, self.template, context)
+
+	def post(self,request):
 		form = PostForm(request.POST)
 		#rose for commenting
 		
@@ -79,10 +85,11 @@ def create(request):
 			context = {
 				"form":form
 			}
-			return render(request,'create.html',context)
+			return render(request,self.template,context)
 
-def details(request,slug):
-	if request.method=="POST":
+class details(View):
+	def post(self,request,slug):
+
 		#rose for commenting
 		task = get_object_or_404(Post,slug=slug)
 		post = PostForm(request.POST,instance = task)
@@ -97,7 +104,7 @@ def details(request,slug):
 			}
 			return render(request,'details.html',context)
 
-	if request.method == "GET":
+	def get(self,request,slug):
 		task = get_object_or_404(Post,slug=slug)
 		form = PostForm(instance = task)
 		context ={
@@ -107,9 +114,8 @@ def details(request,slug):
 		}
 		return render(request, 'details.html', context)
 		#rose for commenting
-
-def delete(request,slug):
-	if request.method=="POST":
+class delete(View):
+	def post(self,request,slug):
 		task = get_object_or_404(Post,slug=slug)
 		task.delete()
 		return redirect("posts:index")
